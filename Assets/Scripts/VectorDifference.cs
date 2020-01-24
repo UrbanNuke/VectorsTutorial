93efa64c37517f400c85942269f08fea8310f3be
urbanz91 <turbino4ka91@gmail.com>
2020-01-24 22:37:53 +0300

    added: vectors difference

---
+++ Assets/Scripts/VectorDifference.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorDifference : MonoBehaviour
+{
+    public GameObject secondCube;
+    public Vector3 toSecondCubeVector;
+
+    [ReadOnly]
+    public Vector3 differenceVector;
+
+    private void OnDrawGizmos() {
+        secondCube.transform.position = toSecondCubeVector;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, toSecondCubeVector);
+        Gizmos.color = Color.blue;
+        differenceVector = toSecondCubeVector - transform.position;
+        Gizmos.DrawRay(transform.position, differenceVector);
+    }
+}

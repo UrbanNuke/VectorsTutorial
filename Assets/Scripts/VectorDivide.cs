77dacde16d49450d7e9fa7adae9e31ecece617d6
urbanz91 <turbino4ka91@gmail.com>
2020-01-25 11:36:19 +0300

    added: scalar multiplication and division vectors

---
+++ Assets/Scripts/VectorDivide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorDivide : MonoBehaviour
+{
+    public float scalar;
+    public Vector3 vectorForDivide;
+    [ReadOnly]
+    public Vector3 resultVector;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        resultVector = vectorForDivide / scalar;
+        Gizmos.DrawRay(transform.position, resultVector);
+    }
+}

f9c92124b260894633a32867690e3739f8b0fe9a
urbanz91 <turbino4ka91@gmail.com>
2020-01-25 12:40:43 +0300

    added: cross product

---
+++ Assets/Scripts/VectorCrossProduct.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorCrossProduct : MonoBehaviour {
+    public Vector3 firstVector;
+    public Vector3 secondVector;
+
+    [ReadOnly]
+    public Vector3 crossProduct;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, firstVector);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, secondVector);
+
+        crossProduct = Vector3.Cross(firstVector, secondVector);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, crossProduct);
+    }
+}

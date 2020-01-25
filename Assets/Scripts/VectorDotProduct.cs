5586f63a4e80bc4961c93f17ac0e62cc5e1a24fc
urbanz91 <turbino4ka91@gmail.com>
2020-01-25 11:40:48 +0300

    added: dot product

---
+++ Assets/Scripts/VectorDotProduct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorDotProduct : MonoBehaviour
+{
+    public GameObject secondCube;
+    public Vector3 firstVector;
+    public Vector3 secondVector;
+    [ReadOnly]
+    public float resultScalar;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, firstVector);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(secondCube.transform.position, secondVector);
+        resultScalar = Vector3.Dot(firstVector.normalized, secondVector.normalized);
+    }
+}
